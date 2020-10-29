39049d933da380b9f025b15feb509dc3a0750164
Brian Ingles <github@emeraldwalk.com>
2020-10-28 23:55:02 -0500

    Fixed bug

--- src/stories/components/ApiControls.stories.tsx
+++ src/stories/components/ApiControls.stories.tsx
@@ -22,7 +22,7 @@ export default {
 } as Meta
 
 const Template: Story<ApiControlsProps> = ({ apiFunction, onChange }) => {
-  const [values, setValues] = useArgValues(apiFunction.args)
+  const { values, setValues } = useArgValues(apiFunction.args)
   const onChangeInternal = React.useCallback(
     (values) => {
       setValues(values)
